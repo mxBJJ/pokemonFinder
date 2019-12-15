36d8a9605cc3298ccd01ef6931255e32d10f8dca
Max Mendes <maxmendes0509@gmail.com>
2019-12-15 18:44:25 -0300

    Initial Commit

---
+++ PokemonFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonFinder
+//
+//  Created by Max Mendes on 15/12/19.
+//  Copyright © 2019 Max Mendes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
